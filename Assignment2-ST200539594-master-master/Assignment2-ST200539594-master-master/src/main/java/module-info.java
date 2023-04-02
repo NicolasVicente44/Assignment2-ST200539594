cab8ffe1dd1a98a2c7e3c4d731b411290bd07b8e
Nick Vicente <nicolas.a.vicente44@gmail.com>
2023-04-02 19:34:41 -0400

    Added model classes for the song and riff as well as validation for their instance variables

--- Assignment2-ST200539594-master-master/Assignment2-ST200539594-master-master/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.assignment2st200539594 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.assignment2st200539594 to javafx.fxml;
-    exports com.example.assignment2st200539594;
-}
